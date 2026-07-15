4cf6f786f19e1bfe6f150827c45e531e99a95c44
erre3333333 <m1978xd@163.com>
2026-07-15 20:42:43 +0800

    feat: frontend/src/App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="app">
+    <router-view />
+    <van-tabbar v-model="active" route>
+      <van-tabbar-item icon="checked" to="/">打卡</van-tabbar-item>
+      <van-tabbar-item icon="bar-chart-o" to="/stats">统计</van-tabbar-item>
+      <van-tabbar-item icon="cloud-o" to="/weather">天气</van-tabbar-item>
+      <van-tabbar-item icon="setting-o" to="/settings">设置</van-tabbar-item>
+    </van-tabbar>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const active = ref(0)
+</script>
+
+<style scoped>
+.app {
+  min-height: 100vh;
+  background: var(--bg);
+  padding-bottom: 50px;
+}
+</style>
