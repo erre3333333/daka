ef1aceeb137cc841e5f279a68e1eb27e8401e3b7
erre3333333 <m1978xd@163.com>
2026-07-16 10:53:15 +0800

    chore: remove backend/models/__init__.py

--- backend/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .schemas import (
-    ScheduleBase, ScheduleCreate, ScheduleUpdate,
-    CheckinBase, CheckinCreate, CheckinDelete,
-    StatsResponse, WeatherResponse,
-    AIAnalyzeRequest, AIAnalyzeResponse
-)
